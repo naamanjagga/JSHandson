d2bf8c44ab9fe7fbc9efe5517303d701b854a813
naamanjagga <namanjagga@cedcommerce.com>
2022-02-09 14:43:47 +0530

    Subtract One Number from Another with JavaScript

--- src/index.js
+++ src/index.js
@@ -39,3 +39,5 @@ fact = "is awesome!";
 console.log(FCC, fact);    //Declare a Read-Only Variable with the const Keyword
 
 const sum = 10 + 10;   //Add Two Numbers with JavaScript
+
+const difference = 45 - 33; //Subtract One Number from Another with JavaScript
