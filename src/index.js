e1fbf5378b616c5e58970979ab62e7a2de06df20
naamanjagga <namanjagga@cedcommerce.com>
2022-02-09 14:44:41 +0530

    Multiply Two Numbers with JavaScript

--- src/index.js
+++ src/index.js
@@ -41,3 +41,5 @@ console.log(FCC, fact);    //Declare a Read-Only Variable with the const Keyword
 const sum = 10 + 10;   //Add Two Numbers with JavaScript
 
 const difference = 45 - 33; //Subtract One Number from Another with JavaScript
+
+const product = 8 * 10;   //Multiply Two Numbers with JavaScript
