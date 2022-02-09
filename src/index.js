0ac6944e3c1dd6a09f24fe0dee065e40c0827193
naamanjagga <namanjagga@cedcommerce.com>
2022-02-09 17:26:59 +0530

    Stand in Line

--- src/index.js
+++ src/index.js
@@ -251,6 +251,23 @@ function processArg(num) {
 
  processed = processArg(7);   //Assignment with a Returned Value
 
+ function nextInLine(arr, item) {
+    // Only change code below this line
+    arr.push(item);
+  
+    return arr.shift();
+    // Only change code above this line
+  }
+  
+  // Setup
+  const testArr = [1, 2, 3, 4, 5];
+  
+  // Display code
+  console.log("Before: " + JSON.stringify(testArr));
+  console.log(nextInLine(testArr, 6));
+  console.log("After: " + JSON.stringify(testArr));  //Stand in Line
+
+
 
 
 
