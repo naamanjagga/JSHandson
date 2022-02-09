2d5c456801f940618004e607977d82d782c6d77f
naamanjagga <namanjagga@cedcommerce.com>
2022-02-09 19:17:07 +0530

    Chaining If Else Statements

--- src/index.js
+++ src/index.js
@@ -440,6 +440,23 @@ function processArg(num) {
   
   orderMyLogic(7);                      //Logical Order in If Else Statements
 
+  function testSize(num) {
+       if (num < 5) {
+    return "Tiny";
+  } else if (num < 10) {
+    return "Small";
+  } else if (num < 15) {
+    return  "Medium";
+  } else if (num < 20) {
+    return  "Large";
+  }else {
+    return "Huge";
+  }
+  
+  }
+  
+  testSize(7);                     //Chaining If Else Statements
+
 
 
 
