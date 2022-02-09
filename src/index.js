9b222ecb23005deb42f77eefdb325d238eaf33b9
naamanjagga <namanjagga@cedcommerce.com>
2022-02-09 16:28:48 +0530

    Access Multi-Dimensional Arrays With Indexes

--- src/index.js
+++ src/index.js
@@ -150,3 +150,12 @@ const myData = myArray[0];     //
 
 const myArray = [18, 64, 99];
 myArray[0] = 45;                  //Modify Array Data With Indexes
+
+const myArray = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [[10, 11, 12], 13, 14],
+  ];
+  
+  const myData = myArray[2][1];  //Access Multi-Dimensional Arrays With Indexes
