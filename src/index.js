2a1bf21e3a1ec25a8e4cdcf89ea2c43c01c73268
naamanjagga <namanjagga@cedcommerce.com>
2022-02-09 16:13:43 +0530

    Word Blanks

--- src/index.js
+++ src/index.js
@@ -133,3 +133,10 @@ const lastName = "Lovelace";
 
 const secondToLastLetterOfLastName = lastName[lastName.length - 2];  //Use Bracket Notation to Find the Nth-to-Last Character in a String
 
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+const wordBlanks = "The " + myNoun + " " + myAdjective + " " + myVerb + " " + myAdverb + "."; 
+//Word Blanks
