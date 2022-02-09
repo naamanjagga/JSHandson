d8e11d56569a6b8f49456d6a484e785dc717d3b3
naamanjagga <namanjagga@cedcommerce.com>
2022-02-09 14:32:17 +0530

    Understanding Uninitialized Variables

--- src/index.js
+++ src/index.js
@@ -13,3 +13,11 @@ var a = 9;  //Initializing Variables with the Assignment Operator
 
 var myFirstName = "Naman";
 var myLastName = "Jagga";     //Declare String Variables
+
+var a = 5;;
+var b = 10;;
+var c = "I am a";
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";   //Understanding Uninitialized Variables
