199965c5841b6b88a61ac43bf6f06b9b3c0f9321
naamanjagga <namanjagga@cedcommerce.com>
2022-02-09 19:12:14 +0530

    Logical Order in If Else Statements

--- src/index.js
+++ src/index.js
@@ -428,6 +428,18 @@ function processArg(num) {
   
   testElseIf(7);                 //Introducing Else If Statements
 
+  function orderMyLogic(val) {
+    if (val < 5) {
+      return "Less than 5";
+    } else if (val < 10) {
+      return "Less than 10";
+    } else {
+      return "Greater than or equal to 10";
+    }
+  }
+  
+  orderMyLogic(7);                      //Logical Order in If Else Statements
+
 
 
 
