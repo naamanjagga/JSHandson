747b8b6ab6eab3b06c3e2b16c65527bcce641c42
naamanjagga <namanjagga@cedcommerce.com>
2022-02-09 14:47:24 +0530

    Increment a Number with JavaScrip

--- src/index.js
+++ src/index.js
@@ -45,3 +45,6 @@ const difference = 45 - 33; //Subtract One Number from Another with JavaScript
 const product = 8 * 10;   //Multiply Two Numbers with JavaScript
 
 const quotient = 66 / 33; //Divide One Number by Another with JavaScript
+
+let myVar = 87;
+myVar++;           //Increment a Number with JavaScrip
