8b0c41ec438101a48e739460fe14bfcffc5d2487
naamanjagga <namanjagga@cedcommerce.com>
2022-02-10 09:38:20 +0530

    Replacing If Else Chains with Switch

--- src/index.js
+++ src/index.js
@@ -554,6 +554,32 @@ function sequentialSizes(val) {
   return answer;
 }                             //Multiple Identical Options in Switch Statements
 
+function chainToSwitch(val) {
+  let answer = "";
+  
+  switch(val) {
+  case "bob":
+    answer = "Marley";
+    break;
+  case 42:
+    answer = "The Answer";
+    break;
+  case 1:
+    answer = "There is no #1";
+    break;
+  case 99:
+    answer = "Missed me by this much!";
+    break;
+  case 7:
+    answer = "Ate Nine";
+    break;
+}
+
+  return answer;
+}
+
+chainToSwitch(7);        //Replacing If Else Chains with Switch
+
 sequentialSizes(1);
 
 
