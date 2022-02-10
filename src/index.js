56c3b528f5ce1b7ceb7b2be747381bf3510e7dd4
naamanjagga <namanjagga@cedcommerce.com>
2022-02-10 10:30:38 +0530

    Accessing Object Properties with Dot Notation

--- src/index.js
+++ src/index.js
@@ -645,6 +645,16 @@ const myDog = {
   
 };                             //Build JavaScript Objects
 
+const testObj = {
+  "hat": "ballcap",
+  "shirt": "jersey",
+  "shoes": "cleats"
+};
+
+
+const hatValue = testObj.hat;    
+const shirtValue = testObj.shirt;   //Accessing Object Properties with Dot Notation
+
 
 
 
