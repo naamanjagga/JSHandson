ba9f219990b622e66468965051ca2ddca41c673e
naamanjagga <namanjagga@cedcommerce.com>
2022-02-10 13:18:58 +0530

    Use the parseInt Function with a Radix

--- src/index.js
+++ src/index.js
@@ -871,6 +871,13 @@ function convertToInteger(str) {
 
 convertToInteger("56");            //Use the parseInt Function
 
+unction convertToInteger(str) {
+  const a = parseInt(str, 2);
+  return a;
+}
+
+convertToInteger("10011");  //Use the parseInt Function with a Radix
+
 
 
 
