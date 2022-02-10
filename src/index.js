49a21c5829cf294b76243518f3988cbfa1f317ae
naamanjagga <namanjagga@cedcommerce.com>
2022-02-10 09:33:05 +0530

    Multiple Identical Options in Switch Statements

--- src/index.js
+++ src/index.js
@@ -530,6 +530,32 @@ function switchOfStuff(val) {
   return answer;
 }                            //Adding a Default Option in Switch Statements
 
+function sequentialSizes(val) {
+  let answer = "";
+ 
+    switch(val) {
+       case 1:
+       case 2:
+       case 3:
+          answer = "Low";
+          break;
+       case 4:
+       case 5:
+       case 6:
+          answer = "Mid";
+          break;
+       case 7:
+       case 8:
+       case 9:
+          answer = "High";
+          break;
+}
+
+  return answer;
+}                             //Multiple Identical Options in Switch Statements
+
+sequentialSizes(1);
+
 
 
 
